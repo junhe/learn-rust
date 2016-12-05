b94df5c90ac62c1d59ad67a6ce737f4b68cbe457
Jun He <jhe@cs.wisc.edu>
2016-12-05 10:42:28 -0600

    Add files

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use std::ptr;
+use std::fs;
+use std::io::{Write, SeekFrom, Seek};
+use std::os::unix::prelude::AsRawFd;
+use mmap::{MemoryMap, MapOption};
+
+// from crates.io
+extern crate mmap;
+extern crate libc;
+
+fn main() {
+    let size: usize = 1024*1024;
+
+    let mut f = fs::OpenOptions::new().read(true)
+                                      .write(true)
+                                      .create(true)
+                                      .open("test.mmap")
+                                      .unwrap();
+
+    // Allocate space in the file first
+    f.seek(SeekFrom::Start(size as u64)).unwrap();
+    f.write_all(&[0]).unwrap();
+    f.seek(SeekFrom::Start(0)).unwrap();
+
+    let mmap_opts = &[
+        // Then make the mapping *public* so it is written back to the file
+        MapOption::MapNonStandardFlags(libc::consts::os::posix88::MAP_SHARED),
+        MapOption::MapReadable,
+        MapOption::MapWritable,
+        MapOption::MapFd(f.as_raw_fd()),
+    ];
+
+    let mmap = MemoryMap::new(size, mmap_opts).unwrap();
+
+    let data = mmap.data();
+
+    if data.is_null() {
+        panic!("Could not access data from memory mapped file")
+    }
+
+    let src = "Hello!";
+    let src_data = src.as_bytes();
+
+    unsafe {
+        ptr::copy(src_data.as_ptr(), data, src_data.len());
+    }
+}
+
+
+
+
+
+
+
+
+
